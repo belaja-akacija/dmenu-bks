0d3ba9e8d70d948dc37a4619196b2a80410becef
belaja-akacija <oselskyi@protonmail.com>
2023-07-07 16:56:26 -0400

    chore: cleanup

--- main.lisp
+++ main.lisp
@@ -1,7 +1,5 @@
 ;;;; open a pdf in zathura in my bookmarks folder from dmenu
 
-;;; TODO be able to type ".." in dmenu and go back a directory
-
 (defparameter *documents* '("*.PDF" "*.pdf" "*.djvu"))
 (defparameter *images* '("*.PNG" "*.png" "*.jpg" "*.jpeg"))
 (defparameter *audio* '("*.wav" "*.WAV" "*.MP3" "*.mp3" "*.ogg" "*.OGG"))
@@ -16,7 +14,7 @@
 
 (defun show-dir (path)
   "Shows the current directory in dmenu"
-  (let* ((path-list-raw (cl-fad:list-directory path))
+  (let* ((path-list-raw (fad:list-directory path))
         (path-length (format nil "~s" (length path-list-raw)))
         (path-list (format nil "~{~A~%~}" path-list-raw))
         (tmp #P "/tmp/bks.tmp"))
@@ -27,16 +25,16 @@
 
 (defun follow-path (path cwd)
   "Follows directories and sends paths to (send-file)"
-  (let ((parent (cl-fad:pathname-directory-pathname cwd)))
+  (let ((parent (fad:pathname-directory-pathname cwd)))
   (cond ((or (string-equal path "..") (string-equal path "...") (string-equal path "../")) ; just in case a file name weirdly has multiple dots in it
          (follow-path (show-dir (if (null path) ; for some reason, from main, "path" is null at first, so this is necessary
                                     (merge-pathnames parent (pathname ".."))
                                     parent))
-                      (cl-fad:pathname-parent-directory parent)))
-        ((null (cl-fad:directory-pathname-p path))
+                      (fad:pathname-parent-directory parent)))
+        ((null (fad:directory-pathname-p path))
          (send-file path))
-        ((cl-fad:directory-pathname-p path)
-         (follow-path (show-dir path) (cl-fad:pathname-parent-directory path))))))
+        ((fad:directory-pathname-p path)
+         (follow-path (show-dir path) (fad:pathname-parent-directory path))))))
 
 (defun send-file (path)
   "Send the directory path to the program that opens that type of file"
@@ -44,15 +42,15 @@
     (cond
      ((find path *documents* :test #'pathname-match-p)
       (launch-zathura path)
-      (follow-path (show-dir curr-path) (cl-fad:pathname-directory-pathname curr-path)))
+      (follow-path (show-dir curr-path) (fad:pathname-directory-pathname curr-path)))
      ((find path *images* :test #'pathname-match-p)
       (launch-sxiv path)
-      (follow-path (show-dir curr-path) (cl-fad:pathname-directory-pathname curr-path))) ; go back to the current directory you were just in, in dmenu, after closing sxiv
+      (follow-path (show-dir curr-path) (fad:pathname-directory-pathname curr-path))) ; go back to the current directory you were just in, in dmenu, after closing sxiv
      ((find path *audio* :test #'pathname-match-p)
-      (if (cl-fad:file-exists-p (merge-pathnames #P "/usr/bin/" (pathname *player*)))
+      (if (fad:file-exists-p (merge-pathnames #P "/usr/bin/" (pathname *player*)))
           (launch-player *terminal* *player* path)
           (launch-player *terminal* "ffplay" path))
-      (follow-path (show-dir curr-path) (cl-fad:pathname-directory-pathname curr-path)))
+      (follow-path (show-dir curr-path) (fad:pathname-directory-pathname curr-path)))
      (t (format t "suitable program not found.")))))
 
 (defun main ()
